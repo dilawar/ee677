499db17780fc60208a2d05e8420f7f080ac94188
Savant Krishna <savant.2020@gmail.com>
2012-08-24 05:12:53 +0530

    Template of the main algo function: to be filled

--- Assignment01/python/qm.py
+++ Assignment01/python/qm.py
@@ -38,6 +38,36 @@ class QuineMcClusky:
             print line[11:len(line)-2].split(',')
             return
 
+    def do_quinemcclusky(self):
+        self.reducedterms=[]
+        self.minterms=self.mintermstobinary()
+        #Change the representation from integers to bits corresponding to variables
+        #After getting the representation, apply the algo.
+        #To implement the algorithm
+
+    def mintermstobinary(self):
+        newminterms=[]
+        #To change self.minterm to bits
+        for term in self.minterms:
+            newminterms.append(self.tobinary(term))
+        return newminterms
+
+    def tobinary(self,term):
+        binaryterm=[]
+        for i in range(self.vars):
+            binaryterm.append(term%2)
+            term=term/2
+        return binaryterm[::-1]
+
+    def print_reduced_terms(self):
+        for e in self.reducedterms:
+            print e
+
+
 if __name__=="__main__":
     #call a instance of the class QuineMcClusky with the filename of minterms.txt
     qm=QuineMcClusky("../clang/minterms.txt")
+    #call the do_quinemcclusky method which will compute the reducedterms
+    qm.do_quinemcclusky()
+    #print the reduceedterms
+    qm.print_reduced_terms()
