ad15636df2c92b5d3587c91ee3ae68edd100a2c4
Savant Krishna <savant.2020@gmail.com>
2012-08-24 05:11:38 +0530

    Method to get the minterms from file

--- Assignment01/python/qm.py
+++ Assignment01/python/qm.py
@@ -12,6 +12,32 @@ class QuineMcClusky:
             self.filename=raw_input("Filename :")
         self.getminterms()
 
+    def getminterms(self):
+        line=self.fin.readline()
+        try:
+            assert line[:7]=="vars = "
+        except AssertionError:
+            print "Not correct format! Need \'vars = <>\'"
+            return
+        try:
+            self.vars=int(line[7:])
+        except ValueError:
+            print "Not an integer variable number!"
+            return
+        line=self.fin.readline()
+        try:
+            assert line[:11]=="minterms = "
+        except AssertionError:
+            print "Not correct format! Need \'minterms = <>\'"
+            return
+        try:
+            self.minterms=map(int,line[11:len(line)-2].split(','))
+            #Eg : minterms = 0,1,2,5,7,8,9,10,13,15,
+        except ValueError:
+            print "Not got integers as minterms! Error!"
+            print line[11:len(line)-2].split(',')
+            return
+
 if __name__=="__main__":
     #call a instance of the class QuineMcClusky with the filename of minterms.txt
     qm=QuineMcClusky("../clang/minterms.txt")
