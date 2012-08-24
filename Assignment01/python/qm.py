e769a30ecf2feea11fea86fb3a3f43004d778600
Savant Krishna <savant.2020@gmail.com>
2012-08-24 05:30:01 +0530

    added authorship and license info

--- Assignment01/python/qm.py
+++ Assignment01/python/qm.py
@@ -1,3 +1,16 @@
+# Quine-McClusky Implementation
+# Written by Savant Krishna "savant.2020@gmail.com"
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
 
 class QuineMcClusky:
     def __init__(self,filename):
