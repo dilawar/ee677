4152f383192d96e54c4bc25758ce514118e15514
Pyro Villainicus <dilawar.iitb@gmail.com>
2012-09-03 10:41:08 +0530

    Intermediate commit.Going to lab to work.

--- Assignment01/haskell/test.hs
+++ Assignment01/haskell/test.hs
@@ -1,10 +1,10 @@
 -- Test script.
 --
-
+import Data.Bits
 import Data.Set as Set 
 
-data Bit = T | F | X deriving (Eq, Show)
-type BitVector = [Bit]
+data Bit = F | T | X deriving (Eq, Ord, Show)
+type BitVector = [Bit] 
 
 
 -- Convert an integer to its minimal bit representation.
@@ -17,11 +17,11 @@ intToBitVector n = (intToBitVector (rem n 2)) ++ (intToBitVector (div n 2))
 -- = 4) rather than just 10 (minimal length).
 intToBitVectorOfLength :: Integer -> Int -> BitVector
 intToBitVectorOfLength num len 
-    | 2 ^ len < num = error "Number can't be represented in given bits."
-    | otherwise = let x = fromIntegral . length $ (intToBitVector num) in helper x num where 
+    | (shiftL 1 len-1) < num = error "Number can't be represented in given bits."
+    | otherwise = let x = length $ (intToBitVector num) in helper x num where 
                     helper x num 
-                        | x >= num = intToBitVector num 
-                        | otherwise = (intToBitVector num) ++ [F | y <- [1 .. (toInteger len - x)]]
+                        | x >= len = intToBitVector num 
+                        | otherwise = (intToBitVector num) ++ [F | y <- [1 .. (len - x)]]
 
 
 hammingDistance :: BitVector -> BitVector -> Int
@@ -36,4 +36,11 @@ hammingDistance (a:as) (b:bs)
                         | otherwise = helper xs ys count
 
 
+-- Convert list of integers to a set of bit-vectors.
+listToMinterms :: [Integer] -> Int -> Set BitVector
+listToMinterms [] n = empty
+listToMinterms (x:xs) n = insert (intToBitVectorOfLength x n) (listToMinterms xs n)
 
+
+-- Now we have the list of minterms, combine those terms which are 1 hamming
+-- distance away.
