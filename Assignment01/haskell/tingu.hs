26dd69ccafac7e131f81f83ba0f67b80d02b899c
Dilawar <dilawar@ee.iitb.ac.in>
2012-09-05 09:24:59 +0530

    Parsing etc is complete.

--- Assignment01/haskell/tingu.hs
+++ Assignment01/haskell/tingu.hs
@@ -29,9 +29,14 @@ processArgs x = do
 
 -- Write the parser here.
 
+--minterms = endBy line eol 
+--line = ((string "vars") <|> (string "minterms")) >> spaces >> char '=' >> spaces >> many1 (char ',' <|> digit)
 minterms = endBy line eol 
-line = ((string "vars") <|> (string "minterms")) >> spaces >> char '=' >> spaces >> many1 (char ',' <|> digit)
-eol = string "\n" <|> string "\n\r"
+line = varline <|> termsline
+varline = ((string "vars") >> spaces >> char '=' >> spaces) >>  (many1 digit)
+termsline = ((string "minterms") >> spaces >> char '=' >> spaces) >> many1 (char ',' <|> digit)
+eol = (string "\n" <|> string "\n\r")
+terms = endBy (many1 digit) (char ',')
 
 computeMinimalForm :: [Flag] -> IO ()
 computeMinimalForm x = do 
@@ -40,13 +45,28 @@ computeMinimalForm x = do
         Left e -> do putStrLn "Error parsing input : "
                      print e 
         Right r -> do 
-            minterms <- getMinterms r
-            putStrLn $ show minterms
+            let var = read (head r) :: Int
+            let t = head (tail r)
+            case parse terms "(unknown)" t of
+                Left e -> do 
+                            print "Error parsing minterms. Make sure each term is ended by ,"
+                            print e
+                Right r -> do 
+                            let terms = map (\y -> read y :: Integer) r
+                            let m = quine terms var
+                            putStrLn "I have found the minimal representation."
+                            print m
+                          
     
 
 getMinterms (x:y) = do 
-    print $ show (x)
-    print $ show (concat y)
+    print (x)
+    case (parse terms "(unknown)" (concat y)) of 
+        Left e -> do putStrLn "Error parsing minterms : "
+                     print e 
+        Right r -> do 
+                    return r
+                    putStrLn ""
 
 main = do 
     args <- getArgs
